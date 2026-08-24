a7d12b5a223d6c101689cb6486f97fa61e090ba4
Avik Das <avikd957@gmail.com>
2026-08-24 15:43:43 +0530

    Step 8: relu_backward

--- model.py
+++ model.py
@@ -95,8 +95,10 @@ def linear_backward(d_out, x, w):
 
     return dx, dw, db
 
-# Step 8 - relu_backward (not yet solved)
-# TODO: implement
+# Step 8 - relu_backward
+def relu_backward(d_out, z):
+    """Backpropagate through ReLU using the pre-activation z."""
+    return d_out * (z > 0)
 
 # Step 9 - first_linear_backward (not yet solved)
 # TODO: implement
